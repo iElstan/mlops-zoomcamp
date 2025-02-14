ca03eaeaec64b2f56f798d44f2ff171e5be7c38f
Roman Peklov <roman_peklov@epam.com>
2025-02-14 14:24:58 +0400

    Adding 02-experiment-tracking homework files

---
+++ 02-experiment-tracking/train.py
@@ -0,0 +1,44 @@
+import os
+import pickle
+import click
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+
+import mlflow
+
+print(mlflow.get_tracking_uri())
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+    # Start an MLflow run
+    with mlflow.start_run():
+
+        # Enable MLflow autologging for sklearn
+        mlflow.sklearn.autolog()
+
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
+
+        # Log RMSE metric
+        mlflow.log_metric("rmse", rmse)
+
+
+if __name__ == '__main__':
+    run_train()
